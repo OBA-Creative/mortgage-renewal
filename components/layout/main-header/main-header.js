4a824aa440a32a3bf5b36a5583582fe1d832cfde
OBA-Creative <uros@obacreative.ca>
2025-10-23 00:09:34 -0700

    Minor updates on the main menu

--- components/layout/main-header/main-header.js
+++ components/layout/main-header/main-header.js
@@ -6,8 +6,8 @@ import NavLink from "./nav-link";
 export default function MainHeader() {
   return (
     <header className="px-32">
-      <div className="flex items-center justify-between py-6 mx-auto text-black max-w-7xl">
-        <Link href="/">
+      <div className="flex items-center justify-between pt-4 pb-6 mx-auto text-black max-w-7xl">
+        <Link href="https://mortgage-renewals.webflow.io/">
           <Image
             src={logo}
             alt="Mortgage Renewals Logo"
@@ -16,10 +16,25 @@ export default function MainHeader() {
           />
         </Link>
         <nav>
-          <ul className="flex space-x-[6px] ">
-            <NavLink href="/" label="Home" />
-            <NavLink href="/faqs" label="FAQs" />
-            <NavLink href="/contact" label="Contact" />
+          <ul className="flex space-x-[12px] items-center">
+            <NavLink
+              href="https://mortgage-renewals.webflow.io/"
+              label="Home"
+            />
+            <NavLink
+              href="https://mortgage-renewals.webflow.io/frequently-asked-question"
+              label="FAQs"
+            />
+            <NavLink
+              href="https://mortgage-renewals.webflow.io/contact"
+              label="Contact"
+            />
+            <Link
+              href="/"
+              className="px-5 py-2 ml-1 font-semibold text-blue-600 transition-colors duration-200 border-2 border-blue-600 rounded-full cursor-pointer hover:bg-blue-500 hover:border-blue-500 hover:text-white"
+            >
+              Renew Now
+            </Link>
           </ul>
         </nav>
       </div>
