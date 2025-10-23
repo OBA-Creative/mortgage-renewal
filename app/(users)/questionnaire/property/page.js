a6ec0a863cddc245bb7575e865d0740fea311c34
OBA-Creative <uros@obacreative.ca>
2025-10-22 17:35:50 -0700

    Minor bug fixes

--- app/(users)/questionnaire/property/page.js
+++ app/(users)/questionnaire/property/page.js
@@ -139,7 +139,7 @@ export default function PropertyPage() {
         <div className="flex justify-end">
           <button
             type="submit"
-            className="px-12 py-3 font-semibold text-white bg-blue-600 rounded-full hover:bg-blue-500"
+            className="px-12 py-3 font-semibold text-white bg-blue-600 rounded-full cursor-pointer hover:bg-blue-500 "
           >
             Continue
           </button>
